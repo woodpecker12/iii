dc04a61e68b31f6818a68eeb05f2277276dcf26d
王卓辉10192452 <wang.zhuohui@zte.com.cn>
2017-11-17 11:52:22 +0800

    add sprint 2 ft

--- test/Sprint2Test.cpp
+++ test/Sprint2Test.cpp
@@ -9,5 +9,14 @@ struct Sprint2Test : testing::Test
 TEST_F(Sprint2Test, should_report_ROCK_when_num_include_string_3)
 {
 	ASSERT_EQ(RockPaperScissors(3).report(), ROCK);
+}
+
+TEST_F(Sprint2Test, should_report_ROCK_when_num_include_string_3_and_can_div_3_and_5)
+{
 	ASSERT_EQ(RockPaperScissors(35).report(), ROCK);
+}
+
+TEST_F(Sprint2Test, should_report_ROCK_when_num_include_string_3_and_can_div_3_and_7)
+{
+	ASSERT_EQ(RockPaperScissors(63).report(), ROCK);
 }
